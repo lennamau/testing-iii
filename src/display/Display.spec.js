717f7007042350a3b916be72111eb6c79d296599
Lenna <lenna@lennamau.com>
2019-03-06 14:18:04 -0700

    snapshot on load test written

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each';
+import Display from './Display'
+import Dashboard from '../dashboard/Dashboard'
+import Controls from '../controls/Dashboard'
+
+
+describe('<Display />', () => {
+    it('matches snapshot', () => {
+      const tree = renderer.create(<Display />) 
+      expect(tree.toJSON()).toMatchSnapshot()
+    });
+  
+})
